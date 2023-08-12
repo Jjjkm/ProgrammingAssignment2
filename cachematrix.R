8728002fe07424156d31da6fc158938c1c3ae3a5
XM <yxm233233@gmail.com>
2023-08-12 21:30:20 +0800

    q4

--- cachematrix.R
+++ cachematrix.R
@@ -132,7 +132,7 @@ rankhospital<-function(state,outcome,num="best"){
     
 }
 
-assign3--------------------------
+##assign3--------------------------
     rankall <- function(outcome, num = "best") {
         ## Read outcome data.
         data<-read.csv("outcome-of-care-measures.csv",
